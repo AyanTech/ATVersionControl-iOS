ee27b2a3f5d46f337fa51bf878af937b2de6f951
Amirhosein Memarian <35375629+amirhoseinmrn@users.noreply.github.com>
2026-07-13 17:23:44 +0330

    chore(build): update sdk version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ATVersionControl",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v12),
     ],
     products: [
         .library(
